4654e4bc2172a5b3d6411650ed624a1c6c79aecc
yanikisia <kathlyn.yary@gmail.com>
2022-01-03 19:36:03 -0300

    testando evergreen design sistem

--- carver-pokedex/src/pages/HomePage/HomePage.js
+++ carver-pokedex/src/pages/HomePage/HomePage.js
@@ -1,10 +1,11 @@
 import React from "react"
-
+import { Button, Pane, Text, majorScale } from 'evergreen-ui'
 const HomePage = () => {
   return (
-    <div >
-      HomePage
-    </div>
+    <Pane display="flex" alignItems="center" marginX={majorScale(2)}>
+    <Button>Click me!</Button>
+    <Text>This is a clickable Butto</Text>
+  </Pane>
   );
 }
 
