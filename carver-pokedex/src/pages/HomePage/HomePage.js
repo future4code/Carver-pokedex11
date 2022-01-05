bca6bce10362aa2e0145a38d857b54ed88bd5b37
yanikisia <kathlyn.yary@gmail.com>
2022-01-05 15:55:20 -0300

    pequenas modificações na estilizaçao

--- carver-pokedex/src/pages/HomePage/HomePage.js
+++ carver-pokedex/src/pages/HomePage/HomePage.js
@@ -3,12 +3,16 @@ import {goToPkedes} from '../../route/coordinator'
 import { MainContainer,HeaderHome,Card,CardImage,CardButtons, AreaCard, StyledButton1,StyledButton2} from "./Styled";
 import {MdCatchingPokemon} from 'react-icons/md'
 import {ImInfo} from 'react-icons/im'
+import { useHistory } from "react-router-dom";
+import Pokedex from "../Pokedex/Pokedex";
+
 const HomePage = () => {
+  const history=useHistory()
   return (
     <MainContainer >
       <HeaderHome>
       Lista de Pokemons
-      <button onClick={()=>goToPkedes}>Pokedex</button>
+      <button onClick={()=>goToPkedes(history)}>Pokedex</button>
       </HeaderHome>
      <AreaCard>
      <Card>
--- carver-pokedex/src/route/coordinator.js
+++ carver-pokedex/src/route/coordinator.js
@@ -2,9 +2,11 @@
 export const goToHomePage = (history) => {
     history.push("/")
 }
+
 export const goToPkedes = (history) => {
     history.push("/pokedex")
 }
+
 export const goToPokemonDetails = (history, id) => {
     history.push(`/pokemon/${id}`)
 }
