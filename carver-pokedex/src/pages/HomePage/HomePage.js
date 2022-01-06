1abb753092c5f08b84d6ad868fff012a50011271
yanikisia <kathlyn.yary@gmail.com>
2022-01-06 20:01:48 -0300

    parte do home finalizada, requisiçoes e funcionalidades de adicionar na pokedex feita

--- carver-pokedex/src/pages/HomePage/HomePage.js
+++ carver-pokedex/src/pages/HomePage/HomePage.js
@@ -1,35 +1,46 @@
-import React, { useContext } from "react"
-import { goToHomePage, goToPokedex,goToPokemonDetails } from '../../route/coordinator'
+import React, { useContext, useEffect, useState } from "react"
+import { goToHomePage, goToPokedex, goToPokemonDetails } from '../../route/coordinator'
 import { MainContainer, HeaderHome, Card, CardImage, CardButtons, AreaCard, StyledButton1, StyledButton2 } from "./Styled";
 import { MdCatchingPokemon } from 'react-icons/md'
 import { ImInfo } from 'react-icons/im'
 import { useHistory } from "react-router-dom";
-import { useState } from "react"; 
 import GlobalState from "../../context/GlobalContext/GlobalState";
 import GlobalStateContext from "../../context/GlobalContext/GlobalStateContext";
 
+
 const HomePage = () => {
   const history = useHistory()
-  const detaisPokemons = useContext(GlobalStateContext)
-  let [pokedex, setPokedex] = useState([])
-  console.log("detais",detaisPokemons)
- 
-   
-    const resultPoke = detaisPokemons && detaisPokemons.map((res) => {
-      console.log(res.sprites.front_default)
+
+  const [detaisPokemons, poke, setPoke] = useContext(GlobalStateContext)
+
+
+
+  const addPoke = (name, photo) => {
+    const indexPoke = detaisPokemons.findIndex((pokes) => { return pokes.name === name })
+    detaisPokemons[indexPoke].isPokedex = true
+    const newPokedex = [...poke, { name, photo }]
+    setPoke(newPokedex)
+
+  }
+
+  const resultPoke = detaisPokemons.map((res) => {
+
+    if (res.isPokedex == false) {
       return (
-        <Card>
+        <Card key={res.name}>
           <CardImage>
-            <img src={res.sprites.front_default} alt={res.name}/>
+            <img src={res.sprites.front_default} alt={res.name} />
             <p>{res.name}</p>
           </CardImage>
           <CardButtons>
-            <StyledButton2 ><MdCatchingPokemon size="2.5em" /> </StyledButton2>
-            <StyledButton1 onClick={()=> goToPokemonDetails(history,res.name) } ><ImInfo size="2.2em" /></StyledButton1>
+            <StyledButton2 onClick={() => addPoke(res.name, res.sprites.front_default)}  ><MdCatchingPokemon size="2.5em" /> </StyledButton2>
+            <StyledButton1 onClick={() => goToPokemonDetails(history, res.name)} ><ImInfo size="2.2em" /></StyledButton1>
           </CardButtons>
         </Card>)
-    })
- 
+    }
+
+  })
+
 
 
   return (
@@ -39,7 +50,7 @@ const HomePage = () => {
         <button onClick={() => goToPokedex(history)}>Pokedex</button>
       </HeaderHome>
       <AreaCard>
-    {resultPoke}
+        {resultPoke}
       </AreaCard>
     </MainContainer>
   );
